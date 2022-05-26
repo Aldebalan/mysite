2d338c43818849afebc309876555e6360a5f02a3
Aldebalan <tjdgus4093@naver.com>
2022-05-26 09:22:42 +0900

    remova all cache

--- mysite02/src/main/java/com/douzone/mysite/web/mvc/guestbook/DeleteAction.java
+++ mysite02/src/main/java/com/douzone/mysite/web/mvc/guestbook/DeleteAction.java
@@ -6,7 +6,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.douzone.mysite.repository.GuestBookRepository;
-import com.douzone.mysite.vo.GuestBookVo;
 import com.douzone.web.mvc.Action;
 import com.douzone.web.util.WebUtil;
 
