f152d7a2c8dfff256bb4746929f351ee8e83e78a
Aldebalan <tjdgus4093@naver.com>
2022-05-30 11:21:37 +0900

    listener&filter

--- mysite02/src/main/java/com/douzone/mysite/controller/GuestBookController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/GuestBookController.java
@@ -14,7 +14,9 @@ public class GuestBookController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
+		// request.setCharacterEncoding("utf-8");
+		// Encoding Filter에서 처리
+		
 		String actionName = request.getParameter("a");
 		
 		ActionFactory factory = new GuestBookActionFactory();
