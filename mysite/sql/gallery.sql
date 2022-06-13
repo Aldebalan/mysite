3444ad4e84ac3ab31817c3fa443e2e8f594a9028
Aldebalan <tjdgus4093@naver.com>
2022-06-13 17:20:36 +0900

    WTF

--- mysite/sql/gallery.sql
+++ mysite/sql/gallery.sql
@@ -4,4 +4,14 @@ desc site;
 
 insert into site value(null, 'mysite', '안녕하세요.   김성현 삽질 블로그에 오신 것을 환영합니다.', '/assets/gallery/default.jpg', '이 사이트는  웹 프로그램밍 실습과제 예제 사이트입니다.\n메뉴는  사이트 소개, 방명록, 게시판이 있구요. JAVA 수업 + 데이터베이스 수업 + 웹프로그래밍 수업 배운 거 있는거 없는 거 다 합쳐서만들어 놓은 사이트 입니다.');
 
-select * from site;
+select * from user;
+
+select title,
+			welcome_message as welcomeMessage,
+			profile_url as profileURL,
+			description
+		from site;
+        
+alter table user add column role enum('USER', 'ADMIN') default 'USER' after gender;
+update user set role='ADMIN' where no=1;
+
